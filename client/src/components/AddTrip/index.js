c6863877719d62178eb2e01a1aa0b85adc10add2
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-05-31 14:41:23 -0500

    error msg

--- client/src/components/AddTrip/index.js
+++ client/src/components/AddTrip/index.js
@@ -1,12 +1,18 @@
 import React, {useRef, useState} from 'react';
+import Alert from 'react-bootstrap/Alert';
 
 function AddTrip () {
     const cityRef = useRef()
     const startDateRef = useRef()
     const endDateRef = useRef()
+    const [error, setError] = useState('');
 
     function handleSubmit (e) {
         e.preventDefault();
+
+        if (endDateRef.current.value < startDateRef.current.value) {
+            return setError("End date must be after star date")
+        }
 
         console.log(cityRef.current.value)
         console.log(startDateRef.current.value);
@@ -18,6 +24,8 @@ function AddTrip () {
        <div>
            <div className="container mt-5">
            <form onSubmit={handleSubmit}>
+                <h1 className="h3 mb-3 fw-normal">Add Trip</h1>
+                {error && <Alert variant= "danger">{error}</Alert>}
                 <div className="mb-3">
                     <label className="form-label">Destination</label>
                     <input ref={cityRef} type="city" className="form-control" placeholder="e.g. New York, Miami" />
