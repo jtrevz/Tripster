7f5b45af83f59456e916da169a5671f208a22f7a
Saebr <bensaebr@gmail.com>
2021-06-01 13:09:18 -0500

    Finished navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -31,44 +31,55 @@ function NavBar() {
 
   return (
   <div>
-
-
 <nav class="navbar-collapse navbar-light bg-light">
 <div className="p-3">
-<h2 className="text-center display-6"><img src="./images/Logo.png" width="50" height="50" class="d-inline-block mb-2 mr-1" alt=""/>Tripster</h2>      
+<h2 className="text-center display-6"><img src="./images/Logo.png" width="50" height="50" class="d-inline-block mb-2 mr-1" alt=""/><Link to="/">Tripster</Link></h2>      
 </div>
+<div className="text-center text-white" id="contact-me">
+        <div className="container">
+            <a
+              className="btn btn-link btn-floating btn-lg text-secondary m-1"
+              role="button"
+              data-mdb-ripple-color="dark"
+              ><button type="button" className="btn btn-light"><Link to="/signup">Signup</Link></button></a>
+            <a
+              className="btn btn-link btn-floating btn-lg text-secondary m-1"
+              role="button"
+              data-mdb-ripple-color="dark"
+              ><button type="button" className="btn btn-light"><Link to="/login">Login</Link></button></a>
+            <a
+              className="btn btn-link btn-floating btn-lg text-secondary m-1"
+              role="button"
+              data-mdb-ripple-color="dark"
+              ><button type="button" className="btn btn-light"><Link to="/profile">Profile</Link></button></a>
+            <a
+              className="btn btn-link btn-floating btn-lg text-secondary m-1"
+              role="button"
+              data-mdb-ripple-color="dark"
+              ><button type="button" className="btn btn-light"><Link to="/trip-planner">New Trip</Link></button></a>
+        </div>
+      </div>
 </nav>
-<nav class="stroke">
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Downloads</a></li>
-      <li><a href="#">More</a></li>
-      <li><a href="#">Nice staff</a></li>
-    </ul>
-  </nav>
 
-
-
-    <Navbar bg="light" expand="lg">
+    {/* <Navbar bg="light" expand="lg">
       <Navbar.Brand><Link to="/">Tripster</Link></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item >{currentUser ? currentUser.email :<Link to="/signup">Sign Up</Link>}</NavDropdown.Item>
-            {currentUser ? <NavDropdown.Item onClick={handleLogout}>Sign Out</NavDropdown.Item> :<Link to="/login">Log In</Link>}
+            {currentUser ? <NavDropdown.Item onClick={handleLogout}>Sign Out</NavDropdown.Item> :<Link to="/login">Log In</Link>} */}
             {/* <NavDropdown.Item >Profile</NavDropdown.Item>
             <NavDropdown.Item >Create a New Trip</NavDropdown.Item>
             <NavDropdown.Divider /> */}
-          </NavDropdown>
+          {/* </NavDropdown>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
         </Form>
       </Navbar.Collapse>
-    </Navbar>
+    </Navbar> */}
   </div>
     );
   }
