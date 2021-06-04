1b617c61f2286aadf97a9a56baed7a93e9c87b2e
Saebr <bensaebr@gmail.com>
2021-06-04 11:51:51 -0500

    Added less depressing photo cards

--- client/src/pages/SplashPage.js
+++ client/src/pages/SplashPage.js
@@ -37,7 +37,7 @@ function SplashPage(props) {
           </div>
     
           <div className="col">
-            <div className="projectcard card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style={{backgroundImage: `url('./images/airport-screen.png')`}}>
+            <div className="projectcard card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style={{backgroundImage: `url('./images/road.jpg')`}}>
               <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
                 <a><h2 className="pt-5 mt-5 mb-4 display-6 lh-1 textshadow">Placeholder</h2></a>
                 <ul className="d-flex list-unstyled mt-auto">
